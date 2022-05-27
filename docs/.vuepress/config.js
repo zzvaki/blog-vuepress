504fd9cfe6b9f5c61628e542866543faacd2e902
zhangzheng <zzvaki@gmail.com>
2022-05-27 15:45:37 +0800

    更新目录，删除lock

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,40 @@
+const { defaultTheme } = require("@vuepress/theme-default");
+
 module.exports = {
   // 站点配置
-  lang: 'zh-CN',
-  title: '一个小博客',
-  description: '这是我的第一个 VuePress 站点',
-  head: [['link', { rel: 'icon', href: '/images/tou.jpg' }]],
+  lang: "zh-CN",
+  title: "一个小博客",
+  description: "这是我的第一个 VuePress 站点",
+  head: [["link", { rel: "icon", href: "/images/tou.jpg" }]],
 
   // 主题和它的配置
-  themeConfig: {
-    logo: '/images/tou.jpg',
+  // themeConfig: {
+  //   logo: "/images/tou.jpg",
+
+  //   navbar: [
+  //     { text: "Home", link: "/" },
+  //     { text: "前端开发", link: "/front-end/index.md" },
+  //     { text: "区块链", link: "/blockchain/index.md" },
+  //     // { text: 'External', link: 'https://google.com' },
+  //   ],
+
+  //   sidebar: {
+  //     "/front-end/": [
+  //       "websocket的简单封装.md",
+  //       "重复请求取消组件.md",
+  //       "vue3开发体验分享.md",
+  //     ],
+  //     "/blockchain/": ["Solidity函数修饰符.md"],
+  //   },
+  // },
+
+  theme: defaultTheme({
+    logo: "/images/tou.jpg",
 
     navbar: [
-      { text: 'Home', link: '/' },
-      { text: '前端开发', link: '/front-end/index.md', },
-      { text: '区块链', link: '/blockchain/index.md' },
+      { text: "Home", link: "/" },
+      { text: "前端开发", link: "/front-end/index.md" },
+      { text: "区块链", link: "/blockchain/index.md" },
       // { text: 'External', link: 'https://google.com' },
     ],
 
@@ -22,11 +44,7 @@ module.exports = {
         "重复请求取消组件.md",
         "vue3开发体验分享.md",
       ],
-      "/blockchain/": [
-        "Solidity函数修饰符.md",
-      ],
-    }
-
-  },
-
-}
+      "/blockchain/": ["Solidity函数修饰符.md"],
+    },
+  }),
+};
